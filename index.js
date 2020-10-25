62e08cb3c462dc8407e7dbcb80d8fe031102883b
Elektroplayer <vitaly.hawanski@yandex.ru>
2020-10-25 10:45:04 +0300

    0.0.0.1

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 //  Дароу
+const discord  = require('discord.js');
+//const ms        = require('ms');
+//const strftime  = require('strftime');
+//const fs        = require('fs');
+//const mongoose  = require('mongoose');
+
+//  Подключение файлов
 const CONFIG = require('./config.json');
+
+//  Константы
+const bot = new discord.Client();
+
+//  Переменные
+let prefix = "e."
+
+bot.login(CONFIG.token); //  Логиним бота
+
+bot.on("ready", () => {
+    console.log('Готов!');
+});
+
+bot.on("message", async (message) => {
+    if(message.content.startsWith(prefix + "help")) message.channel.send('Не знаю, чем тебе помочь')
+});
