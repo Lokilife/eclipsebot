3e6168e0eff19f07d21eeae37a77bc9665d84943
Elektroplayer <vitaly.hawanski@yandex.ru>
2020-10-25 15:26:21 +0300

    0.0.0.2

---
+++ cmd/help.js
@@ -0,0 +1,22 @@
+module.exports = {
+    run: (bot,message,args,con)=> {try{
+        if(args[0] == "help") {
+            return message.channel.send(con.defEmb.setTitle("Тебе серьёзно нужна помощь по команде помощи?").setDescription("Мне лень)").setFooter(con.footer))
+        }
+
+        let categories = con.categories;
+        let emb = con.defEmb.setTitle('Помощь')
+        let text =''
+        for(let i1=0;i1<=categories.length-1;i1++) {
+            text = ''
+            for(let i2=0;i2<=con.cmds.length-1;i2++) {
+                if(con.cmds[i2].category == categories[i1]) text = text+`**${con.prefix}${con.cmds[i2].cmd}** - ${con.cmds[i2].desc}\n`
+            }
+            emb.addField(categories[i1],text);
+        }
+        message.channel.send(emb.setFooter(con.footer));
+    }catch(err){console.log(err)}},
+    cmd: ["help","?"],
+    desc: "Помощь",
+    category: "Общее"
+}
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //  Дароу
 const discord  = require('discord.js');
 //const ms        = require('ms');
-//const strftime  = require('strftime');
-//const fs        = require('fs');
+//const strftime  = require('strftime').localizeByIdentifier('ru_RU');
+const fs        = require('fs');
 //const mongoose  = require('mongoose');
 
 //  Подключение файлов
@@ -10,16 +10,59 @@ const CONFIG = require('./config.json');
 
 //  Константы
 const bot = new discord.Client();
+bot.commands = new discord.Collection(); // Тут будут храниться команды
 
 //  Переменные
-let prefix = "e."
+let prefix  = "e.";
+let cmds    = [];
+let color   = "525592";
 
 bot.login(CONFIG.token); //  Логиним бота
 
-bot.on("ready", () => {
-    console.log('Готов!');
+fs.readdir("./cmd/", (err, files) => {
+	if(err) console.log(err);
+	let jsfile = files.filter(f => f.split(".").pop() === "js");
+	if(jsfile.length <= 0){
+		console.log("Не могу найти команды!");
+		return;
+	}
+
+	jsfile.forEach((f) =>{
+		let props = require(`./cmd/${f}`);
+		console.log(`${f} загружен!`);
+		if(typeof props.cmd == 'string') bot.commands.set(`${props.cmd}`, props);
+		else if(typeof props.cmd == 'object') {
+			for(let i2=0;i2<=props.cmd.length-1;i2++) {
+				bot.commands.set(props.cmd[i2], props);
+			}
+        }
+        cmds.push({"cmd":props.cmd,"desc":props.desc,"category":props.category})
+	});
 });
 
-bot.on("message", async (message) => {
-    if(message.content.startsWith(prefix + "help")) message.channel.send('Не знаю, чем тебе помочь')
+bot.on("message", async (message) => {try{
+    if(message.author.bot) return; //  Не слушаем других ботов
+    if(message.channel.type == 'dm') return; //  Не слушаем ЛС
+
+    //let messageArray  = message.content.replace(/\s/g, ' ').split(" ");
+    let messageArray  = message.content.replace(/\s+/g, ' ').split(" ");
+	let cmd           = messageArray[0];
+    let args          = messageArray.slice(1);
+
+    let commandfile
+	if(cmd.substr(0, 2) == prefix) commandfile = bot.commands.get(cmd.slice(prefix.length));
+	if(commandfile) commandfile.run(bot,message,args,{
+        "cmds":cmds,
+        "prefix":prefix,
+        "color": color,
+        "defEmb": new discord.MessageEmbed().setColor(color),
+        "footer": message.author.username +' | © Night Devs',
+        "categories": ['Общее'],
+        "moderators": ['449585603567157258','449590549683634176','652500169354510357'],
+        //"logchannel": allSettings.logChannel
+    });
+}catch (err) {console.log(err)}})
+
+bot.on("ready", () => {
+    console.log('Готов!');
 });
