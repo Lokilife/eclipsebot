fc911550eb257d01eda8fc1a0feaa19092d110df
Elektroplayer <vitaly.hawanski@yandex.ru>
2020-10-29 00:19:22 +0300

    0.0.0.10

--- cmd/info.js
+++ cmd/info.js
@@ -8,10 +8,17 @@ module.exports = {
             return message.channel.send(con.defEmb.setTitle("Тебе серьёзно нужна помощь по этой команде?").setDescription("Мне лень)").setFooter(con.footer))
         }
 
+        let up = bot.uptime
+        let ms = up%1000
+        let s = ((up-ms)/1000)%60
+        let m = ((((up-ms)/1000)-s)/60)%60
+        let h = ((((((up-ms)/1000)-s)/60)-m)/60)%60
+        let d = (((((((up-ms)/1000)-s)/60)-m)/60)-h)/24
+
         let embed = con.defEmb
         .setFooter(`Для ${message.author.tag}`)
         .setTitle("Информация о боте")
-        .addField("Основное",`Пользователей: \`${bot.users.cache.size}\`\nСерверов: \`${bot.guilds.cache.size}\`\nДата создания: \`${strftime('%d.%m.%Y год в %H:%M', new Date(bot.user.createdTimestamp))}\``)
+        .addField("Основное",`Пользователей: \`${bot.users.cache.size}\`\nСерверов: \`${bot.guilds.cache.size}\`\nДата создания: \`${strftime('%d.%m.%Y год в %H:%M', new Date(bot.user.createdTimestamp))}\`\nВремя работы: \`${d} : ${h} : ${m} : ${s}.${ms}\``)
         .addField("Техническая информация",`Использование ОЗУ:  \`${(process.memoryUsage().rss / 1024 / 1024)
         .toFixed(2)} МБ\`\nВерсия Node.JS: \`${process.version}\`\nВерсия Discord.JS: \`v${discord.version}\`\nВерсия бота: \`v0.0.0\`\nРазработчики: \`[ElectroPlayer ✔]#0256\`, \`Tegnio#6882\`\nТестеры: \`[Ueuecoyotl]#4032\``)
         .addField("Полезные ссылки", "[Сервер поддержки](https://discord.gg/YM3KMDM) | [GitHub бота](https://github.com/Elektroplayer/eclipsebot) | [Ссылка на бота](https://discord.com/api/oauth2/authorize?client_id=769659625129377812&permissions=872934480&scope=bot)")
--- cmd/ping.js
+++ cmd/ping.js
@@ -5,19 +5,9 @@ module.exports = {
             return message.channel.send(con.defEmb.setTitle("Тебе серьёзно нужна помощь по этой команде?").setDescription("Мне лень)").setFooter(con.footer))
         }
 
-        let up = bot.uptime
-        let ms = up%1000
-        let s = ((up-ms)/1000)%60
-        let m = ((((up-ms)/1000)-s)/60)%60
-        let h = ((((((up-ms)/1000)-s)/60)-m)/60)%60
-        let d = (((((((up-ms)/1000)-s)/60)-m)/60)-h)/24
-
-        let used = process.memoryUsage().rss / 1024 / 1024;
         let resEmbed = con.defEmb
         .setTitle("Понг!")
-        .addField(`Пинг:`, `${Math.round(bot.ws.ping)} ms`,true)
-        .addField(`Выделенные ресурсы:`, `${Math.round(used * 100) / 100} МБ`,true)
-        .addField(`Время работы:`,`${d} : ${h} : ${m} : ${s}.${ms}`,true)
+        .addField(`Пинг:`, `${Math.round(bot.ws.ping)} ms`)
         .setFooter(con.footer)
         message.channel.send(resEmbed);
     }catch(err){
@@ -31,7 +21,7 @@ module.exports = {
         );
         console.log(err)
     }},
-    cmd: ["res","ping"],
+    cmd: ["ping"],
     desc: "Ресурсы и пинг",
     category: "Общее"
 }
